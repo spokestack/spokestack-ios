002be7d94074e49296388aa6fceac12d6b205bd0
Cory D. Wiles <corywiles@icloud.com>
2018-09-28 15:06:43 -0400

    Project meta
    - new classes

---
+++ SpokeStack/SpokeStack/Error.swift
@@ -0,0 +1,14 @@
+//
+//  Error.swift
+//  SpokeStack
+//
+//  Created by Cory D. Wiles on 9/28/18.
+//  Copyright © 2018 Pylon AI, Inc. All rights reserved.
+//
+
+import Foundation
+
+enum AudioError: Error {
+    case general(String)
+    case audioSessionSetup(String)
+}
