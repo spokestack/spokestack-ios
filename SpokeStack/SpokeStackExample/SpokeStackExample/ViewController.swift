4907f92a7ddfe15b1181e9f4569b566718c7577f
Cory D. Wiles <corywiles@icloud.com>
2018-10-04 12:09:10 -0400

    outlets

--- SpokeStack/SpokeStackExample/SpokeStackExample/ViewController.swift
+++ SpokeStack/SpokeStackExample/SpokeStackExample/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var startRecordingButton: UIButton!
+    @IBOutlet weak var resultsLabel: UILabel!
+    @IBOutlet weak var stopRecordingButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
