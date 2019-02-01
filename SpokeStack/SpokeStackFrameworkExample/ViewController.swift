2a1b99c47d43f028cf39d2e00c6d75b2af8f6d33
Cory D. Wiles <corywiles@icloud.com>
2019-01-31 23:14:22 -0500

    added start and stop recording as well as “done” button

--- SpokeStack/SpokeStackFrameworkExample/ViewController.swift
+++ SpokeStack/SpokeStackFrameworkExample/ViewController.swift
@@ -22,20 +22,37 @@ class ViewController: UIViewController {
     // MARK: View Life Cycle
     
     override func viewDidLoad() {
+        
         super.viewDidLoad()
+        self.wakeWordButton.isEnabled = false
     }
-    
     
     @IBAction func googleAction(_ sender: Any) {
     
+        let googleViewController: GoogleViewController = GoogleViewController()
+        let navigationViewController: UINavigationController = UINavigationController(rootViewController: googleViewController)
+        
+        self.present(navigationViewController, animated: true, completion: nil)
     }
-    
-    
+
     @IBAction func appleAxction(_ sender: Any) {
-    }
     
+        let appleViewController: AppleViewController = AppleViewController()
+        let navigationViewController: UINavigationController = UINavigationController(rootViewController: appleViewController)
+        
+        self.present(navigationViewController, animated: true, completion: nil)
+    }
     
     @IBAction func wakeWordAction(_ sender: Any) {
+        
+        let wakeWordViewController: WakeWordViewController = WakeWordViewController()
+        let navigationViewController: UINavigationController = UINavigationController(rootViewController: wakeWordViewController)
+        
+        self.present(navigationViewController, animated: true, completion: nil)
+    }
+    
+    @objc func dismissViewController(_ sender: Any?) -> Void {
+        self.dismiss(animated: true, completion: nil)
     }
 }
 
--- SpokeStack/SpokeStackFrameworkExample/WakeWordViewController.swift
+++ SpokeStack/SpokeStackFrameworkExample/WakeWordViewController.swift
@@ -23,11 +23,36 @@ struct WKWordConfiguration: WakeRecognizerConfiguration {
 
 class WakeWordViewController: UIViewController {
     
-    @IBOutlet weak var startRecordingButton: UIButton!
+    lazy var startRecordingButton: UIButton = {
+        
+        let button: UIButton = UIButton(frame: .zero)
+        
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Start Recording", for: .normal)
+        button.addTarget(self,
+                         action: #selector(WakeWordViewController.startRecordingAction(_:)),
+                         for: .touchUpInside)
+        
+        button.setTitleColor(.blue, for: .normal)
+        
+        return button
+    }()
     
-    @IBOutlet weak var stopRecordingButton: UIButton!
-    
-    @IBOutlet weak var resultsLabel: UILabel!
+    var stopRecordingButton: UIButton = {
+        
+        let button: UIButton = UIButton(frame: .zero)
+        
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Stop Recording", for: .normal)
+        button.addTarget(self,
+                         action: #selector(WakeWordViewController.stopRecordingAction(_:)),
+                         for: .touchUpInside)
+        
+        button.setTitleColor(.blue, for: .normal)
+        
+        
+        return button
+    }()
     
     lazy private var pipeline: SpeechPipeline = {
         
@@ -38,35 +63,64 @@ class WakeWordViewController: UIViewController {
                                    delegate: self)
     }()
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    override func loadView() {
+        
+        super.loadView()
+        self.view.backgroundColor = .white
+        self.title = "WakeWord"
+        
+        let doneBarButtonItem: UIBarButtonItem = UIBarButtonItem(barButtonSystemItem: .done,
+                                                                 target: self,
+                                                                 action: #selector(WakeWordViewController.dismissViewController(_:)))
+        self.navigationItem.rightBarButtonItem = doneBarButtonItem
     }
     
-    @IBAction func startRecordingAction(_ sender: Any) {
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        
+        self.view.addSubview(self.startRecordingButton)
+        self.view.addSubview(self.stopRecordingButton)
+        
+        self.startRecordingButton.centerYAnchor.constraint(equalTo: self.view.centerYAnchor).isActive = true
+        self.startRecordingButton.leftAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leftAnchor).isActive = true
+        self.startRecordingButton.rightAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.rightAnchor).isActive = true
+        
+        self.stopRecordingButton.topAnchor.constraint(equalTo: self.startRecordingButton.bottomAnchor, constant: 50.0).isActive = true
+        self.stopRecordingButton.leftAnchor.constraint(equalTo: self.startRecordingButton.leftAnchor).isActive = true
+        self.stopRecordingButton.rightAnchor.constraint(equalTo: self.startRecordingButton.rightAnchor).isActive = true
+    }
+    
+    @objc func startRecordingAction(_ sender: Any) {
         self.pipeline.start()
     }
     
-    @IBAction func stopRecordingAction(_ sender: Any) {
+    @objc func stopRecordingAction(_ sender: Any) {
         self.pipeline.stop()
+    }
+    
+    @objc func dismissViewController(_ sender: Any?) -> Void {
+        self.dismiss(animated: true, completion: nil)
     }
 }
 
 extension WakeWordViewController: SpeechRecognizer {
     
     func didRecognize(_ result: SPSpeechContext) {
-        //        self.resultsLabel.text = result.transcript
+        print("transcript \(result.transcript)")
     }
     
     func didFinish(_ error: Error?) {
+
         print("didFinish \(String(describing: error))")
-        //        self.stopRecordingButton.isEnabled.toggle()
-        //        self.startRecordingButton.isEnabled.toggle()
+        self.stopRecordingButton.isEnabled.toggle()
+        self.startRecordingButton.isEnabled.toggle()
     }
     
     func didStart() {
         
-        //        self.stopRecordingButton.isEnabled.toggle()
-        //        self.startRecordingButton.isEnabled.toggle()
+        self.stopRecordingButton.isEnabled.toggle()
+        self.startRecordingButton.isEnabled.toggle()
     }
 }
 
