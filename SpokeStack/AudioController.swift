e81b7089241a9a5b2c87e0460cd5dce8f126d6dc
Noel Weichbrodt <noel@weichbrodt.org>
2019-04-19 17:45:19 -0400

    AudioController is no longer a singleton; remove unused code

--- SpokeStack/AudioController.swift
+++ SpokeStack/AudioController.swift
@@ -11,54 +11,9 @@ import AVFoundation
 
 let audioProcessingQueue: DispatchQueue = DispatchQueue(label: "com.pylon.audio.callback")
 
-func recordingCallback(
-    inRefCon: UnsafeMutableRawPointer,
-    ioActionFlags: UnsafeMutablePointer<AudioUnitRenderActionFlags>,
-    inTimeStamp: UnsafePointer<AudioTimeStamp>,
-    inBusNumber: UInt32,
-    inNumberFrames: UInt32,
-    ioData: UnsafeMutablePointer<AudioBufferList>?) -> OSStatus {
-    guard let remoteIOUnit: AudioComponentInstance = AudioController.shared.remoteIOUnit else {
-        return kAudioServicesSystemSoundUnspecifiedError
-    }
-    var status: OSStatus = noErr
-    let channelCount: UInt32 = 1
-    
-    var bufferList = AudioBufferList()
-    bufferList.mNumberBuffers = channelCount
-    
-    let buffers = UnsafeMutableBufferPointer<AudioBuffer>(start: &bufferList.mBuffers,
-                                                          count: Int(bufferList.mNumberBuffers))
-    buffers[0].mNumberChannels = 1
-    buffers[0].mDataByteSize = inNumberFrames * 2
-    buffers[0].mData = nil
-    
-    /// get the recorded samples
-    
-    status = AudioUnitRender(remoteIOUnit,
-                             ioActionFlags,
-                             inTimeStamp,
-                             inBusNumber,
-                             inNumberFrames,
-                             UnsafeMutablePointer<AudioBufferList>(&bufferList))
-    if status != noErr {
-        return status
-    }
-    
-    let data: Data = Data(bytes: buffers[0].mData!, count: Int(buffers[0].mDataByteSize))
-    
-    audioProcessingQueue.sync {
-        AudioController.shared.delegate?.processSampleData(data)
-    }
-    
-    return noErr
-}
-
 class AudioController {
     
     // MARK: Public (properties)
-    
-    static let shared: AudioController = AudioController()
     
     weak var delegate: AudioControllerDelegate?
     weak var pipelineDelegate: PipelineDelegate?
@@ -94,8 +49,9 @@ class AudioController {
         }
     }
     
-    init() {
+    init(delegate: PipelineDelegate?) {
         print("AudioController init")
+        self.pipelineDelegate = delegate
         switch AVAudioSession.sharedInstance().category {
         case AVAudioSession.Category.record:
             break
@@ -108,7 +64,6 @@ class AudioController {
                                                selector: #selector(audioRouteChanged),
                                                name: AVAudioSession.routeChangeNotification,
                                                object: AVAudioSession.sharedInstance())
-        self.prepareRemoteIOUnit()
     }
     
     // MARK: Public functions
@@ -196,71 +151,5 @@ class AudioController {
             print("AudioController audioRouteChanged new category: " + session.category.rawValue)
         default: ()
         }
-    }
-    
-    @discardableResult
-    private func prepareRemoteIOUnit() -> OSStatus {
-        
-        // MARK: prepare RemoteIO unit component
-        
-        var status: OSStatus = noErr
-        let remoteIOComponent = AudioComponentFindNext(nil, &audioComponentDescription)
-        status = AudioComponentInstanceNew(remoteIOComponent!, &remoteIOUnit)
-        if status != noErr {
-            return status
-        }
-        
-        // MARK: Configure the RemoteIO unit for input
-        
-        let bus1: AudioUnitElement = 1
-        var oneFlag: UInt32 = 1
-        status = AudioUnitSetProperty(self.remoteIOUnit!,
-                                      kAudioOutputUnitProperty_EnableIO,
-                                      kAudioUnitScope_Input,
-                                      bus1,
-                                      &oneFlag,
-                                      UInt32(MemoryLayout<UInt32>.size));
-        if status != noErr {
-            return status
-        }
-        
-        // MARK: set format for mic input (bus 1) on RemoteIO unit's output scope
-        var asbd: AudioStreamBasicDescription = AudioStreamBasicDescription()
-        asbd.mSampleRate = Double(self.sampleRate)
-        asbd.mFormatID = kAudioFormatLinearPCM
-        asbd.mFormatFlags = kAudioFormatFlagIsSignedInteger | kAudioFormatFlagIsPacked
-        asbd.mBytesPerPacket = 2
-        asbd.mFramesPerPacket = 1
-        asbd.mBytesPerFrame = 2
-        asbd.mChannelsPerFrame = 1
-        asbd.mBitsPerChannel = 16
-        status = AudioUnitSetProperty(self.remoteIOUnit!,
-                                      kAudioUnitProperty_StreamFormat,
-                                      kAudioUnitScope_Output,
-                                      bus1,
-                                      &asbd,
-                                      UInt32(MemoryLayout<AudioStreamBasicDescription>.size))
-        if (status != noErr) {
-            return status
-        }
-        
-        // MARK: Set the recording callback
-        
-        var callbackStruct: AURenderCallbackStruct = AURenderCallbackStruct()
-        callbackStruct.inputProc = recordingCallback
-        callbackStruct.inputProcRefCon = nil
-        status = AudioUnitSetProperty(self.remoteIOUnit!,
-                                      kAudioOutputUnitProperty_SetInputCallback,
-                                      kAudioUnitScope_Global,
-                                      bus1,
-                                      &callbackStruct,
-                                      UInt32(MemoryLayout<AURenderCallbackStruct>.size));
-        if status != noErr {
-            return status
-        }
-        
-        // MARK: Initialize the RemoteIO unit
-        
-        return AudioUnitInitialize(self.remoteIOUnit!)
     }
 }
