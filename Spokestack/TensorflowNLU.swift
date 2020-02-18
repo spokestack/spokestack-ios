1fa492fd98061454440244029591e55bdf70a164
Noel Weichbrodt <noel@weichbrodt.org>
2020-02-18 13:19:52 -0500

    perform nlu classification asynchronously

--- Spokestack/TensorflowNLU.swift
+++ Spokestack/TensorflowNLU.swift
@@ -94,95 +94,104 @@ import TensorFlowLite
     /// Classifies the provided input. The classifciation results are sent to the instance's configured NLUDelegate.
     /// - Parameter input: The NLUInput to classify.
     @objc public func classify(utterance: String, context: [String : Any]) -> Void {
-        do {
-            let prediction = try self.classify(utterance) as NLUResult
-            self.delegate?.classification(result: prediction)
-        } catch let error {
-            self.delegate?.failure(error: error)
+        DispatchQueue.main.async {
+            let prediction = self.classify(utterance)
+            switch prediction {
+            case .success(let classification): self.delegate?.classification(result: classification)
+            case .failure(let error):
+                self.delegate?.failure(error: error)
+            }
         }
     }
     
     /// Classifies the provided input. NLUResult is sent to all subscribers.
     /// - Parameter inputs: The NLUInput to classify.
+    /// - Warning: `classify` is resource-intensive and should be used with `subscribe(on:)` to ensure it is not blocking the UI thread.
     @available(iOS 13.0, *)
-    public func classify(inputs: [String]) ->  Publishers.Sequence<[NLUResult], Never> {
-        return inputs.map { try! self.classify($0) }.publisher
+    public func classify(inputs: [String]) -> Publishers.Sequence<[Result<NLUResult, Error>], Never> {
+        return inputs.map
+            { self.classify($0) }
+        .publisher
         //return AnyPublisher<[Prediction], Error>(try inputs.map { try self.predict($0) as Prediction })
         //return Publishers.First(upstream: Just([Prediction(intent: "", confidence: 0.0, slots: [:])]).setFailureType(to: Error.self)).eraseToAnyPublisher()
     }
     
-    private func classify(_ input: String) throws -> NLUResult {
-        guard let model = self.interpreter else {
-            throw NLUError.model("NLU model was not initialized.")
-        }
-        guard let tokenizer = self.tokenizer else {
-            throw NLUError.tokenizer("Tokenizer was not initialized.")
-        }
-        guard let metadata = self.metadata else {
-            throw NLUError.metadata("Metadata was not initialized.")
-        }
-        guard let maxInputTokenLength = self.maxTokenLength else {
-            throw NLUError.invalidConfiguration("NLU model maximum input tokens length was not set.")
-        }
-        
-        // preprocess the model inputs
-        //  tokenize + encode the input, terminate the utterance with the terminator token, and  pad from the end of the utterance up to the expected input size (128 32-bit ints)
-        var encodedInput = try tokenizer.tokenizeAndEncode(input)
-        encodedInput.append(self.terminatorToken)
-        encodedInput += Array(repeating: self.paddingToken, count: maxInputTokenLength - encodedInput.count)
-        
-        // downcast the (assumed iOS) default Int64 to match the model's expected Int32 size. This is safe because the model vocabulary code indicies are 32-bit.
-        let downcastEncodedInput = encodedInput.map({ Int32(truncatingIfNeeded: $0) })
-        _ = try downcastEncodedInput
-            .withUnsafeBytes({
-                try model.copy(Data($0), toInputAt: InputTensors.input.rawValue)})
-        
-        // run the model over the provided inputs
-        try model.invoke()
-        
-        // process the model's output
-        // extract, decode + detokenize the classified intent, then hydrate the intent result object based on the provided model metadata.
-        let encodedIntentsTensor = try model.output(at: OutputTensors.intent.rawValue)
-        let encodedIntents = encodedIntentsTensor.data.toArray(type: Float32.self, count: encodedIntentsTensor.data.count/4)
-        let intentsArgmax = encodedIntents.argmax()
-        if intentsArgmax.0 > metadata.model.intents.count {
-            throw NLUError.model("NLU model returned an intent value outside the expected range.")
-        }
-        let intent = metadata.model.intents[intentsArgmax.0]
-        
-        // extract, decode + detokenize the classified tags, then hydrate the result slots based on the provided model metadata.
-        let encodedTagTensor = try model.output(at: OutputTensors.tag.rawValue)
-        let encodedTags = encodedTagTensor.data.toArray(type: Float32.self, count: encodedTagTensor.data.count/4)
-        // the posteriors for the tags are grouped by the number of model metadata tags, so stride through them calculating the argmax for each stride.
-        let encodedTagsArgmax = stride(from: 0,
-                                       to: encodedTags.count,
-                                       by: metadata.model.tags.count)
-            .map({
-                Array(encodedTags[$0..<$0+metadata.model.tags.count]).argmax()
-                
+    private func classify(_ input: String) -> Result<NLUResult, Error> {
+        do {
+            guard let model = self.interpreter else {
+                throw NLUError.model("NLU model was not initialized.")
+            }
+            guard let tokenizer = self.tokenizer else {
+                throw NLUError.tokenizer("Tokenizer was not initialized.")
+            }
+            guard let metadata = self.metadata else {
+                throw NLUError.metadata("Metadata was not initialized.")
+            }
+            guard let maxInputTokenLength = self.maxTokenLength else {
+                throw NLUError.invalidConfiguration("NLU model maximum input tokens length was not set.")
+            }
+            
+            // preprocess the model inputs
+            //  tokenize + encode the input, terminate the utterance with the terminator token, and  pad from the end of the utterance up to the expected input size (128 32-bit ints)
+            var encodedInput = try tokenizer.tokenizeAndEncode(input)
+            encodedInput.append(self.terminatorToken)
+            encodedInput += Array(repeating: self.paddingToken, count: maxInputTokenLength - encodedInput.count)
+            
+            // downcast the (assumed iOS) default Int64 to match the model's expected Int32 size. This is safe because the model vocabulary code indicies are 32-bit.
+            let downcastEncodedInput = encodedInput.map({ Int32(truncatingIfNeeded: $0) })
+            _ = try downcastEncodedInput
+                .withUnsafeBytes({
+                    try model.copy(Data($0), toInputAt: InputTensors.input.rawValue)})
+            
+            // run the model over the provided inputs
+            try model.invoke()
+            
+            // process the model's output
+            // extract, decode + detokenize the classified intent, then hydrate the intent result object based on the provided model metadata.
+            let encodedIntentsTensor = try model.output(at: OutputTensors.intent.rawValue)
+            let encodedIntents = encodedIntentsTensor.data.toArray(type: Float32.self, count: encodedIntentsTensor.data.count/4)
+            let intentsArgmax = encodedIntents.argmax()
+            if intentsArgmax.0 > metadata.model.intents.count {
+                throw NLUError.model("NLU model returned an intent value outside the expected range.")
+            }
+            let intent = metadata.model.intents[intentsArgmax.0]
+            
+            // extract, decode + detokenize the classified tags, then hydrate the result slots based on the provided model metadata.
+            let encodedTagTensor = try model.output(at: OutputTensors.tag.rawValue)
+            let encodedTags = encodedTagTensor.data.toArray(type: Float32.self, count: encodedTagTensor.data.count/4)
+            // the posteriors for the tags are grouped by the number of model metadata tags, so stride through them calculating the argmax for each stride.
+            let encodedTagsArgmax = stride(from: 0,
+                                           to: encodedTags.count,
+                                           by: metadata.model.tags.count)
+                .map({
+                    Array(encodedTags[$0..<$0+metadata.model.tags.count]).argmax()
+                    
+                })
+            // decode the tags according to the model metatadata index
+            let tagsByInput = encodedTagsArgmax.map(
+            {
+                metadata.model.tags[$0.0]
             })
-        // decode the tags according to the model metatadata index
-        let tagsByInput = encodedTagsArgmax.map(
-        {
-            metadata.model.tags[$0.0]
-        })
-        // zip up the input + tags, since their ordering corresponds. this also effectivly truncates the tag posteriors by the input size, ignoring all posteriors outside the input length.
-        let inputTagged = zip(encodedInput, tagsByInput) // input:String, tag:String tuple
-        // hyrdate Slot objects according to the zipped input + tag
-        let slots = try inputTagged.reduce([:], { (dict, inputTag) in
-            var filterName = inputTag.1
-            // the model metadata tags are occasionally prefixed with POS tags. But the model metadata slots do not have this prefix. Remove the prefix in order to perform the model metadata slot lookups based on tag.
-            if let prefixIndex = filterName.range(of: "_")?.upperBound {
-                filterName = String(filterName.suffix(from: prefixIndex))
-            }
-            let value = try tokenizer.decodeAndDetokenize([inputTag.0])
-            guard let type = intent.slots.filter({ $0.name == filterName }).first?.type else {
-                return dict
-            }
-            return [filterName : Slot(type: type, value: value)]
-        }) as [String:Slot]
-        
-        // return the classification result
-        return NLUResult(utterance: input, intent: intent.name, confidence: intentsArgmax.1, slots: slots)
+            // zip up the input + tags, since their ordering corresponds. this also effectivly truncates the tag posteriors by the input size, ignoring all posteriors outside the input length.
+            let inputTagged = zip(encodedInput, tagsByInput) // input:String, tag:String tuple
+            // hyrdate Slot objects according to the zipped input + tag
+            let slots = try inputTagged.reduce([:], { (dict, inputTag) in
+                var filterName = inputTag.1
+                // the model metadata tags are occasionally prefixed with POS tags. But the model metadata slots do not have this prefix. Remove the prefix in order to perform the model metadata slot lookups based on tag.
+                if let prefixIndex = filterName.range(of: "_")?.upperBound {
+                    filterName = String(filterName.suffix(from: prefixIndex))
+                }
+                let value = try tokenizer.decodeAndDetokenize([inputTag.0])
+                guard let type = intent.slots.filter({ $0.name == filterName }).first?.type else {
+                    return dict
+                }
+                return [filterName : Slot(type: type, value: value)]
+            }) as [String:Slot]
+            
+            // return the classification result
+            return .success(NLUResult(utterance: input, intent: intent.name, confidence: intentsArgmax.1, slots: slots))
+        } catch let error {
+            return .failure(error)
+        }
     }
 }
