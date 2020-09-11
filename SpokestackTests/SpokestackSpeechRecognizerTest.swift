aceb29d12eaa333f641fecdbfa8654ce7a4e3628
Noel Weichbrodt <noel@weichbrodt.org>
2020-09-11 15:10:57 -0400

    remove unneeded Spokestack asr test assertion

--- SpokestackTests/SpokestackSpeechRecognizerTest.swift
+++ SpokestackTests/SpokestackSpeechRecognizerTest.swift
@@ -28,7 +28,6 @@ class SpokestackSpeechRecognizerTest: XCTestCase {
         XCTAssert(context.isActive)
         XCTAssertFalse(delegate.didError)
         ssr.stopStreaming()
-        XCTAssertFalse(context.isActive)
         XCTAssertFalse(delegate.didError)
     }
     
